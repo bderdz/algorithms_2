ab15b23724eeaeb5308331ea9f39bc3c4e66b248
Bogdan Derdziak <bogdan.derdz@gmail.com>
2025-03-22 20:51:31 +0100

    bst

---
+++ bst4.cpp
@@ -0,0 +1,143 @@
+//#include <iostream>
+//#include <unordered_set>
+//#include <algorithm>
+//#include <vector>
+//
+//struct Node {
+//	int v;
+//	Node *left;
+//	Node *right;
+//
+//	Node(int v) {
+//		this->left = this->right = nullptr;
+//		this->v = v;
+//	}
+//};
+//
+//class BST {
+//private:
+//	Node *root;
+//
+//	Node *trim_bst(Node *root, int a, int b) {
+//		if (!root) return nullptr;
+//
+//		if (root->v < a) {
+//			Node *right_sub = trim_bst(root->right, a, b);
+//			delete root;
+//			return right_sub;
+//		}
+//
+//		if (root->v > b) {
+//			Node *left_sub = trim_bst(root->left, a, b);
+//			delete root;
+//			return left_sub;
+//		}
+//
+//		root->left = trim_bst(root->left, a, b);
+//		root->right = trim_bst(root->right, a, b);
+//
+//		return root;
+//	}
+//
+//public:
+//	BST() {
+//		this->root = nullptr;
+//	}
+//
+//	Node *get_root() {
+//		return root;
+//	}
+//
+//	void add(int v) {
+//		if (root == nullptr) {
+//			root = new Node(v);
+//			return;
+//		}
+//
+//		Node *current = root;
+//		while (current != nullptr) {
+//			if (v < current->v) {
+//				if (current->left == nullptr) {
+//					current->left = new Node(v);
+//					break;
+//				}
+//
+//				current = current->left;
+//			} else {
+//				if (current->right == nullptr) {
+//					current->right = new Node(v);
+//					break;
+//				}
+//
+//				current = current->right;
+//			}
+//		}
+//	}
+//
+//
+//	void trim(int a, int b) {
+//		root = trim_bst(root, a, b);
+//	}
+//
+//	void inorder_vector(Node *root, std::vector<int> &output) {
+//		if (root == nullptr) return;
+//
+//		inorder_vector(root->left, output);
+//		output.push_back(root->v);
+//		inorder_vector(root->right, output);
+//	}
+//
+//	void delete_tree(Node *node) {
+//		if (node != nullptr) {
+//			delete_tree(node->right);
+//			delete_tree(node->left);
+//			delete node;
+//		}
+//	}
+//
+//	~BST() {
+//		delete_tree(root);
+//	}
+//};
+//
+//void find_closest_path(std::vector<int> &paths, int sum) {
+//	std::unordered_set<int> closest;
+//
+//	for (int v: paths) {
+//		auto iter = closest.find(sum - v);
+//		if (iter != closest.end()) {
+//			std::cout << std::min(v, *iter) << " " << std::max(v, *iter) << "\n";
+//			return;
+//		}
+//
+//		closest.insert(v);
+//	}
+//
+//	std::cout << "NIE ZNALEZIONO";
+//}
+//
+//int main() {
+//	int n;
+//	int a, b;
+//	int k;
+//	BST *political_paths = new BST();
+//
+//	std::cin >> n;
+//
+//	for (int i = 0; i < n; i++) {
+//		int v;
+//		std::cin >> v;
+//
+//		political_paths->add(v);
+//	}
+//
+//	std::cin >> a >> b >> k;
+//
+//	political_paths->trim(a, b);
+//	std::vector<int> inorder_paths;
+//	political_paths->inorder_vector(political_paths->get_root(), inorder_paths);
+//	find_closest_path(inorder_paths, k);
+//
+//	delete political_paths;
+//	return 0;
+//}
--- hopkroft3.cpp
+++ hopkroft3.cpp
@@ -12,9 +12,9 @@ struct Point {
 };
 
 struct Vertex {
+	std::vector<int> edges;
 	int idx;
 	int type;
-	std::vector<int> edges;
 	bool is_black;
 	int edge;
 
@@ -50,10 +50,118 @@ public:
 
 public:
 	BipChess(matrix &graph, int n) {
-		this->n = 0;
 		this->max_captures = 0;
+		this->n = convert_to_bipartite(graph, n);
+	}
 
-		convert_to_bipartite(graph, n);
+	void add_captured(int white_idx, int white_type, Vertex &black_v) {
+		black_v.edges.push_back(white_idx);
+		//* Jeżeli nie mamy zmapowanego indeksu, tworzymy wierzchołek i dodajemy krawędź
+		if (index_map.find(white_idx) == index_map.end()) {
+			Vertex captured_v = Vertex(white_idx, white_type, false);
+			captured_v.edges.push_back(black_v.idx);
+
+			index_map.insert({white_idx, bip_graph.size()});
+			bip_graph.push_back(captured_v);
+		} else {
+			bip_graph[index_map[white_idx]].edges.push_back(black_v.idx);
+		}
+	}
+
+	//* Tworzenie listy sąsiedztwa dla wierzchołków
+	void find_all_captures(Vertex &v, int x, int y, matrix &board, int size) {
+		int white_idx;
+		int white_type;
+		switch (v.type) {
+			case 1:
+				//* 3 do przodu
+				if (x + 3 < size && board[y][x + 3]) {
+					white_idx = size * y + x + 3;
+					white_type = board[y][x + 3];
+					add_captured(white_idx, white_type, v);
+				}
+				//* 2 do przodu + 1 lewo/prawo
+				if (x + 2 < size) {
+					if (y - 1 >= 0 && board[y - 1][x + 2]) {
+						white_idx = v.idx - size + 2;
+						white_type = board[y - 1][x + 2];
+						add_captured(white_idx, white_type, v);
+					}
+
+					if (y + 1 < size && board[y + 1][x + 2]) {
+						white_idx = v.idx + size + 2;
+						white_type = board[y + 1][x + 2];
+						add_captured(white_idx, white_type, v);
+					}
+				}
+				break;
+			case 2:
+				//* 1 do przodu
+				if (x + 1 < size && board[y][x + 1]) {
+					white_idx = size * y + x + 1;
+					white_type = board[y][x + 1];
+					add_captured(white_idx, white_type, v);
+				}
+				//* 1 lewo
+				if (y - 1 >= 0 && board[y - 1][x]) {
+					white_idx = x - size;
+					white_type = board[y - 1][x];
+					add_captured(white_idx, white_type, v);
+				}
+				//* 1 prawo
+				if (y + 1 < size && board[y + 1][x]) {
+					white_idx = x + size;
+					white_type = board[y + 1][x];
+					add_captured(white_idx, white_type, v);
+				}
+				break;
+			case 3:
+				//* 1 do przodu
+				if (x + 1 < size && board[y][x + 1]) {
+					white_idx = size * y + x + 1;
+					white_type = board[y][x + 1];
+					add_captured(white_idx, white_type, v);
+				}
+				//* 2 do przodu + 1 lewo/prawo
+				if (x + 2 < size) {
+					if (y - 1 >= 0 && board[y - 1][x + 2]) {
+						white_idx = v.idx - size + 2;
+						white_type = board[y - 1][x + 2];
+						add_captured(white_idx, white_type, v);
+					}
+
+					if (y + 1 < size && board[y + 1][x + 2]) {
+						white_idx = v.idx + size + 2;
+						white_type = board[y + 1][x + 2];
+						add_captured(white_idx, white_type, v);
+					}
+				}
+				break;
+			default:
+				break;
+		}
+	}
+
+	//* Tworzenie grafu dwudzielnego
+	int convert_to_bipartite(matrix &graph, int size) {
+		bip_graph = std::vector<Vertex>();
+		index_map = std::unordered_map<int, int>();
+
+		for (int i = 0; i < size; i++) {
+			for (int j = i % 2; j < size; j += 2) {
+				int figure_type = graph[i][j];
+				if (figure_type) {
+					Vertex figure = Vertex(size * i + j, figure_type, true);
+
+					find_all_captures(figure, j, i, graph, size);
+					if (figure.edges.empty()) continue;
+
+					index_map.insert({figure.idx, bip_graph.size()});
+					bip_graph.push_back(figure);
+				}
+			}
+		}
+		return bip_graph.size();
 	}
 
 	int bfs(std::queue<int> &free) {
@@ -74,7 +182,7 @@ public:
 			Point current = queue.front();
 			queue.pop();
 
-			if (current.d < height) {    //! Przerwać jeśli przekroczyło ?
+			if (current.d < height) {
 				for (int e: bip_graph[current.i].edges) {
 					int nbr_idx = index_map[e];
 					Vertex &nbr = bip_graph[nbr_idx];
@@ -91,37 +199,27 @@ public:
 			} else {
 				break;
 			}
-
-
 		}
-
 		return height;
 	}
 
 	bool dfs(int i, int h, int max_h, bool is_black = true) {
 		if (h < max_h) {
 			for (int e: bip_graph[i].edges) {
-				e = index_map[e];
-				if (dist[e] == dist[i] + 1) {
-					if (dfs(e, h + 1, max_h, !is_black)) {
+				int e_idx = index_map[e];
+
+				if (dist[e_idx] == dist[i] + 1) {
+					if (dfs(e_idx, h + 1, max_h, !is_black)) {
 						if (is_black) {
 							bip_graph[i].edge = e;
-							bip_graph[e].edge = i;
-						} else {
-							bip_graph[i].edge = -1;
+							bip_graph[e_idx].edge = i;
 						}
-
 						return true;
 					}
-
-					return false;
 				}
 			}
-		} else if (h == max_h) {
-			if (bip_graph[i].edge == -1 && !bip_graph[i].is_black) {
-				dist[i] = h;
-				return true;
-			}
+		} else if (h == max_h && bip_graph[i].edge == -1 && !is_black) {
+			return true;
 		}
 		return false;
 	}
@@ -131,17 +229,10 @@ public:
 		std::queue<int> free;
 		int height = 0;
 		max_captures = 0;
+		int current_height;
 
-		int current_height = bfs(free);
-
-		while (current_height > height) {
+		while ((current_height = bfs(free)) > height) {
 			height = current_height;
-			std::cout << "HEIGHT: " << height << " MAX " << max_captures << "\n";
-			for (int i = 0; i < n; i++) {
-				std::cout << dist[i] << " ";
-			}
-
-			std::cout << "\n";
 
 			while (!free.empty()) {
 				int free_v = free.front();
@@ -151,89 +242,8 @@ public:
 					max_captures++;
 				}
 			}
-
-			current_height = bfs(free);
 		}
-		//		while (!free.empty()) {
-		//			std::cout << free.front() << " ";
-		//			free.pop();
-		//		}
-		//		std::cout << "\n";
-
-
-		std::cout << max_captures << "\n";
-
-
 		return max_captures;
-	}
-
-	void add_captured(int white_idx, int white_type, Vertex &black_v) {
-		black_v.edges.push_back(white_idx);
-		//* Jeżeli nie mamy zmapowanego indeksu tworzymy wierzchołek i dodajemy krawędź
-		if (index_map.find(white_idx) == index_map.end()) {
-			Vertex captured_v = Vertex(white_idx, white_type, false);
-			captured_v.edges.push_back(black_v.idx);
-
-			index_map.insert({white_idx, bip_graph.size()});
-			bip_graph.push_back(captured_v);
-		} else {
-			bip_graph[index_map[white_idx]].edges.push_back(black_v.idx);
-		}
-	}
-
-	//* Tworzenie listy sąsiedztwa dla wierzchołków
-	//! DO POPRAWY
-	void find_all_captures(Vertex &v, int x, int y, matrix &board, int size) {
-		switch (v.type) {
-			case 1:
-				//* 3 do przodu
-				if (x + 3 < size && board[y][x + 3]) {
-					int captured_idx = size * y + x + 3;
-					add_captured(captured_idx, board[y][x + 3], v);
-				}
-				//* 2 do przodu + 1 lewo/prawo
-				if (x + 2 < size) {
-					int captured_idx = v.idx - size + 2;
-					if (y - 1 >= 0 && board[y - 1][x + 2]) {
-						add_captured(captured_idx, board[y - 1][x + 2], v);
-					}
-
-					captured_idx = v.idx + size + 2;
-					if (y + 1 < size && board[y + 1][x + 2]) {
-						add_captured(captured_idx, board[y + 1][x + 2], v);
-					}
-				}
-				break;
-			case 2:
-				break;
-			case 3:
-				break;
-			default:
-				break;
-		}
-	}
-
-	//* Tworzenie grafu dwudzielnego
-	void convert_to_bipartite(matrix &graph, int size) {
-		bip_graph = std::vector<Vertex>();
-		index_map = std::unordered_map<int, int>();
-
-		for (int i = 0; i < size; i++) {
-			for (int j = i % 2; j < size; j += 2) {
-				int figure_type = graph[i][j];
-				if (figure_type) {
-					Vertex figure = Vertex(size * i + j, figure_type, true);
-
-					find_all_captures(figure, j, i, graph, size);
-					if (figure.edges.empty()) continue;
-
-					index_map.insert({figure.idx, bip_graph.size()});
-					bip_graph.push_back(figure);
-				}
-			}
-		}
-
-		this->n = bip_graph.size();
 	}
 
 	//* DEBUG
@@ -250,7 +260,7 @@ public:
 	}
 };
 
-
+//! Tragiczny kod ;)
 int main() {
 	std::ios_base::sync_with_stdio(false);
 	std::cout.tie(nullptr);
@@ -258,20 +268,17 @@ int main() {
 
 	int n;
 	std::cin >> n;
-
+	// Wczytywanie szachownicy
 	matrix board(n, std::vector<int>(n));
-
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < n; j++) {
 			std::cin >> board[i][j];
 		}
 	}
-
-
+	// Przetwarzanie
 	BipChess bip_board = BipChess(board, n);
-	bip_board.print_graph();
-	std::cout << bip_board.n << "\n";
-	bip_board.max_association();
-
+	int max_captures = bip_board.max_association();
+	// OUTPUT
+	std::cout << n * n - max_captures << "\n";
 	return 0;
 }
