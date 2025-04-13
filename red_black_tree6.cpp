2a871f1dd9708c07e00ffc5a824450a988136774
Bogdan Derdziak <bogdan.derdz@gmail.com>
2025-04-13 23:29:14 +0200

    Red Black Tree implementation

--- red_black_tree6.cpp
+++ red_black_tree6.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <sstream>
-#include <unordered_map>
 #include <list>
 
 struct ListNode {
@@ -9,24 +8,26 @@ struct ListNode {
 	ListNode(std::string &name) : name(name) {}
 };
 
-typedef std::unordered_map<size_t, std::list<ListNode *>::iterator> node_ptr_map;
+
+struct TreeNode {
+	size_t key;
+	std::list<ListNode *>::iterator value;
+	char color;
+	TreeNode *parent;
+	TreeNode *left;
+	TreeNode *right;
+
+	TreeNode(size_t key) {
+		this->key = key;
+		this->color = 'R';
+		this->left = this->right = this->parent = nullptr;
+	}
+};
 
 class RedBlackTree {
 private:
-	struct TreeNode {
-		size_t key;
-		std::list<ListNode *>::iterator value;
-		char color;
-		TreeNode *parent;
-		TreeNode *left;
-		TreeNode *right;
-
-		TreeNode(size_t key, TreeNode *parent = nullptr) {
-			this->key = key;
-			this->parent = parent;
-			this->left = this->right = nullptr;
-		}
-	};
+	TreeNode *root;
+	TreeNode *NIL;
 
 	void left_rotate(TreeNode *x) {
 		TreeNode *y = x->right;
@@ -56,8 +57,64 @@ private:
 		x->parent = y;
 	}
 
-	TreeNode *root;
-	TreeNode *NIL;
+	void fix_insert(TreeNode *curr) {
+		while (curr != root && curr->parent->color == 'R') {
+			if (curr->parent == curr->parent->parent->left) {
+				TreeNode *uncle = curr->parent->parent->right;
+				if (uncle->color == 'R') {
+					curr->parent->color = 'B';
+					uncle->color = 'B';
+					curr->parent->parent->color = 'R';
+					curr = curr->parent->parent;
+				} else {
+					if (curr == curr->parent->right) {
+						curr = curr->parent;
+						left_rotate(curr);
+					}
+					curr->parent->color = 'B';
+					curr->parent->parent->color = 'R';
+					right_rotate(curr->parent->parent);
+				}
+			} else {
+				TreeNode *uncle = curr->parent->parent->left;
+				if (uncle->color == 'R') {
+					curr->parent->color = 'B';
+					uncle->color = 'B';
+					curr->parent->parent->color = 'R';
+					curr = curr->parent->parent;
+				} else {
+					if (curr == curr->parent->left) {
+						curr = curr->parent;
+						right_rotate(curr);
+					}
+					curr->parent->color = 'B';
+					curr->parent->parent->color = 'R';
+					left_rotate(curr->parent->parent);
+				}
+			}
+		}
+		root->color = 'B';
+	}
+
+	TreeNode *tree_search(TreeNode *node, size_t key) {
+		if (node == NIL || key == node->key) {
+			return node;
+		}
+
+		if (key < node->key) {
+			return tree_search(node->left, key);
+		}
+
+		return tree_search(node->right, key);
+	}
+
+	void cut_tree(TreeNode *node) {
+		if (node == NIL) return;
+		cut_tree(node->left);
+		cut_tree(node->right);
+		delete node;
+	}
+
 public:
 	RedBlackTree() {
 		this->NIL = new TreeNode(-1);
@@ -65,12 +122,49 @@ public:
 		NIL->left = NIL->right = NIL;
 		this->root = NIL;
 	}
+
+	void insert(size_t key, std::list<ListNode *>::iterator &value) {
+		TreeNode *new_node = new TreeNode(key);
+		new_node->value = value;
+		new_node->left = new_node->right = new_node->parent = NIL;
+
+		TreeNode *parent = nullptr;
+		TreeNode *current = root;
+		while (current != NIL) {
+			parent = current;
+			if (new_node->key < parent->key) current = current->left;
+			else current = current->right;
+		}
+
+		new_node->parent = parent;
+		if (parent == nullptr) root = new_node;
+		else if (new_node->key < parent->key) parent->left = new_node;
+		else parent->right = new_node;
+
+		if (new_node->parent == nullptr) {
+			new_node->color = 'B';
+			return;
+		}
+
+		if (new_node->parent->parent == nullptr) return;
+
+		fix_insert(new_node);
+	}
+
+	TreeNode *search(size_t key) {
+		return tree_search(root, key);
+	}
+
+	~RedBlackTree() {
+		cut_tree(root);
+		delete NIL;
+	}
 };
 
 
 class DangerousBoard {
 private:
-	node_ptr_map map;
+	RedBlackTree map;
 	std::list<ListNode *> list;
 
 	size_t key_hash(const std::string &str) {
@@ -83,23 +177,26 @@ private:
 	}
 
 public:
-	DangerousBoard() {}
+	DangerousBoard() {
+		this->map = RedBlackTree();
+	}
 
 	void swap_nodes(std::string &name_a, std::string &name_b) {
 		size_t key_a, key_b;
 		key_a = key_hash(name_a);
 		key_b = key_hash(name_b);
 
-		auto a_it = map[key_a];
-		auto b_it = map[key_b];
-		std::iter_swap(a_it, b_it);
-		std::swap(map[key_a], map[key_b]);
+		TreeNode *a = map.search(key_a);
+		TreeNode *b = map.search(key_b);
+
+		std::iter_swap(a->value, b->value);
+		std::swap(a->value, b->value);
 	}
 
 	void move_node(int offset, std::string &key) {
 		size_t hash_key = key_hash(key);
-		auto node_it = map[hash_key];
-		auto target_it = node_it;
+		auto node = map.search(hash_key);
+		auto target_it = node->value;
 
 		for (int i = 0; i < std::abs(offset); i++) {
 			if (offset > 0) target_it--;
@@ -109,14 +206,14 @@ public:
 		}
 
 		if (offset < 0) target_it++;
-		list.splice(target_it, list, node_it);
-		map[hash_key] = std::prev(target_it);
+		list.splice(target_it, list, node->value);
+		node->value = std::prev(target_it);
 	}
 
 	void add_node(std::string &name) {
 		ListNode *new_node = new ListNode(name);
 		auto node_it = list.insert(list.end(), new_node);
-		map[key_hash(name)] = node_it;
+		map.insert(key_hash(name), node_it);
 	}
 
 	void print_board() {
